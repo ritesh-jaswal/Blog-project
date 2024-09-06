f1c81a77562e1c28cb2ae540baf601a0e25a4c45
ritesh <ritesh.jaswal@testingxperts.com>
2024-09-06 15:07:41 +0530

    ADDED SORTING TO GETALLPOSTS

--- src/main/java/com/example/Blog_project/controllers/PostController.java
+++ src/main/java/com/example/Blog_project/controllers/PostController.java
@@ -54,9 +54,10 @@ public class PostController
     @GetMapping("posts")
     public ResponseEntity<PostResponse> getAllPosts
             (@RequestParam(value = "pageNumber",defaultValue = "0",required = false) Integer pageNumber,
-             @RequestParam(value = "pageSize",defaultValue = "5",required = false)Integer pageSize)
+             @RequestParam(value = "pageSize",defaultValue = "5",required = false)Integer pageSize,
+             @RequestParam(value = "sortBy",defaultValue = "postId",required = false)String sortBy)
     {
-        PostResponse postResponse = this.postService.getAllPosts(pageNumber,pageSize);
+        PostResponse postResponse = this.postService.getAllPosts(pageNumber,pageSize,sortBy);
         return new ResponseEntity<>(postResponse,HttpStatus.OK);
     }
 
--- src/main/java/com/example/Blog_project/serviceimpl/PostServiceImpl.java
+++ src/main/java/com/example/Blog_project/serviceimpl/PostServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -71,9 +72,9 @@ public class PostServiceImpl implements PostService
     }
 
     @Override
-    public PostResponse getAllPosts(Integer pageNumber, Integer pageSize)
+    public PostResponse getAllPosts(Integer pageNumber, Integer pageSize,String sortBy)
     {
-        Pageable pageable = PageRequest.of(pageNumber,pageSize);
+        Pageable pageable = PageRequest.of(pageNumber,pageSize, Sort.by(sortBy).ascending());
 
         Page<Post> postPage = this.postRepo.findAll(pageable);
         List<Post> posts = postPage.getContent();
